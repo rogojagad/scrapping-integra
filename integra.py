8fede583e505f2d403e63f60ca42c48e65851dfc
rogojagad <rogojagadalit@gmail.com>
2018-04-06 15:45:10 +0700

    move common method to module

--- integra.py
+++ integra.py
@@ -1,87 +1,31 @@
-import requests
-import json
 import re
-from bs4 import BeautifulSoup
+from utils import *
 
-def credential():
-    return json.load(open('credential.json'))
-
-def login_integra(s):
-    hal_login = 'https://integra.its.ac.id'
-
-    s.post(hal_login, data=credential())
-
-    res = s.get('http://integra.its.ac.id/dashboard.php')
-
-    if res:
-        print "Login sukses!"
-
-def get_sim_gate(res):
-    html = BeautifulSoup(res.text, "lxml")
-
-    for tags in html.find_all('meta'):
-        content = tags.get('content')
-
-    return content.split('=', 1)[1]
-
-def get_sim_akademik_cookie(s):
-    res = s.get('https://integra.its.ac.id/dashboard.php?sim=AKADX__-__')
-    
-    sim_akademik_gate = get_sim_gate(res)
-
-    s.get(sim_akademik_gate)
-
-    res = s.get('http://akademik3.its.ac.id/home.php')
-
-    if res:
-        print 'Cookie SIM Akademik didapatkan'
-
-def get_sim_beasiswa_cookie(s):
-    res = s.get('https://integra.its.ac.id//dashboard.php?sim=BEA__10__')
-    
-    sim_akademik_gate = get_sim_gate(res)
-
-    s.get(sim_akademik_gate)
-
-    res = s.get('http://beasiswa.its.ac.id/data/home.php')
-
-    if res:
-        print 'Cookie SIM Beasiswa didapatkan'
-
-def get_sim_skem_cookie(s):
-    res = s.get('https://integra.its.ac.id/dashboard.php?sim=SKEM__10__')
-    
-    sim_akademik_gate = get_sim_gate(res)
-
-    s.get(sim_akademik_gate)
-
-    res = s.get('http://akademik3.its.ac.id/skem/petunjukskemmhs.php')
-
-    if res:
-        print 'Cookie SIM SKEM didapatkan'
 
 def get_ipk_rank(s):
     get_sim_akademik_cookie(s)
 
-    for page_num in range(1,41):
+    for page_num in range(1, 41):
         scrap_ipk_rank(s, page_num)
+
 
 def cleanup(data):
     to_string = str(data)
     return re.sub('\s+', ' ', to_string)
 
+
 def scrap_ipk_rank(s, page_num):
     ipk_rank_page = 'http://akademik3.its.ac.id/list_ipkrank.php'
 
-    payload ={
-        'page':page_num
+    payload = {
+        'page': page_num
         # ,'thnAngkatan':'2015'
     }
 
     res = s.post(ipk_rank_page, data=payload)
 
     data_ranking = BeautifulSoup(res.text, 'html.parser')
-    
+
     tabel_mhs = data_ranking.find('table', class_='GridStyle')
 
     rows = tabel_mhs.find_all('tr')
@@ -95,29 +39,31 @@ def scrap_ipk_rank(s, page_num):
             for column in columns_in_a_row:
                 row_data += cleanup(column.get_text())
             print row_data
-            
+
+
 def isi_skem(s):
     get_sim_skem_cookie(s)
 
     form = 'http://akademik3.its.ac.id/skem/entry_realisasi.php'
 
-    nrp = credential()['userid']
+    nrp = json_parser('credential')['userid']
 
     payload = {
-        "semester" : "21142",
-        "kodekegiatan" : "1;35000",
-        "partisipasi" : "Testing",
-        "tglmulai" : "05-03-2018",
-        "tglselesai" : "05-03-2018",
-        "act" : "simpan",
-        "submit" : "submit",
-        "nrp" : nrp
+        "semester": "21142",
+        "kodekegiatan": "1;35000",
+        "partisipasi": "Testing",
+        "tglmulai": "05-03-2018",
+        "tglselesai": "05-03-2018",
+        "act": "simpan",
+        "submit": "submit",
+        "nrp": nrp
     }
 
     res = s.post(form, data=payload)
 
     if res.status_code == 200:
         print "SKEM berhasil diisi"
+
 
 s = requests.Session()
 login_integra(s)
--- stalking.py
+++ stalking.py
@@ -1,47 +1,17 @@
-import requests
-import json
-from bs4 import BeautifulSoup
+from utils import *
 
-def json_parser(filename):
-    return json.load(open(filename))
-
-def login_integra(s):
-    hal_login = 'https://integra.its.ac.id'
-
-    s.post(hal_login, data=json_parser('credential.json'))
-
-    res = s.get('http://integra.its.ac.id/dashboard.php')
-
-    if res:
-        print "Login sukses!"
-
-def get_sim_gate(res):
-    html = BeautifulSoup(res.text, "lxml")
-
-    for tags in html.find_all('meta'):
-        content = tags.get('content')
-
-    return content.split('=', 1)[1]
-
-def get_sim_akademik_cookie(s):
-    res = s.get('https://integra.its.ac.id/dashboard.php?sim=AKADX__-__')
-    
-    sim_akademik_gate = get_sim_gate(res)
-
-    s.get(sim_akademik_gate)
-
-    res = s.get('http://akademik3.its.ac.id/home.php')
-
-    if res:
-        print 'Cookie SIM Akademik didapatkan'
 
 def get_members_page(matkul, kelas):
-    return "http://akademik3.its.ac.id/lv_peserta.php?mkJur=51100&mkID="+matkul+"&mkSem=2&mkThn=2017&mkKelas="+kelas
+    base_url = "http://akademik3.its.ac.id/lv_peserta.php?mkJur=51100&mkID="
+    semester = "&mkSem=2&mkThn=2017&mkKelas="
+
+    return base_url + matkul + semester + kelas
+
 
 def go_stalk(s, nrp):
-    classes = ['A','B','C','D','E','F',]
+    classes = ['A', 'B', 'C', 'D', 'E', 'F', ]
 
-    subjects = json_parser('matkul.json')
+    subjects = json_parser('matkul')
 
     for matkul in subjects:
         for kelas in classes:
@@ -50,6 +20,7 @@ def go_stalk(s, nrp):
 
             if nrp in result.text:
                 print subjects[matkul] + " " + kelas
+
 
 nrp = raw_input("Masukkan NRP yang akan di stalk (gunakan format baru): ")
 
